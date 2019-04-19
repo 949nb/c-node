67853fc022e2b311edba3050dda4fe026b9194e1
hy949 <1661346771@qq.com>
2019-04-19 14:47:49 +0800

    添加了formatDate过滤器

--- src/main.js
+++ src/main.js
@@ -6,18 +6,35 @@ import router from './router'
 import Axios from 'axios'
 
 
-Vue.prototype.$http = Axios
-Vue.config.productionTip = false
+Vue.prototype.$http = Axios;
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
-})
-Vue.filter('formatData',function (str) {
-  if(!str) return ''
-  let date = new Data(str)
-  let time = new Data().getTime() - date.getTime()
-})
+});
+Vue.filter('formatDate', function (str) {
+  if (!str) return '';
+  let date = new Date(str);
+  let time = new Date().getTime() - date.getTime(); //现在的时间 - 传入的时间 = 相差的时间(毫秒)
+  if (time < 0) {
+    return ''
+  } else if ((time / 1000 < 30)) {
+    return '刚刚'
+  } else if ((time / 1000 < 60)) {
+    return parseInt((time / 1000)) + '秒前'
+  } else if ((time / 60000 < 60)) {
+    return parseInt((time / 60000)) + '分钟前'
+  } else if ((time / 3600000 < 24)) {
+    return parseInt((time / 3600000)) + '小时前'
+  } else if ((time / 86400000 < 31)) {
+    return parseInt((time / 86400000)) + '天前'
+  } else if ((time / 2592000000 < 12)) {
+    return parseInt((time / 2592000000)) + '月前'
+  } else {
+    return parseInt((time/31536000000)) + '年前'
+  }
+});
